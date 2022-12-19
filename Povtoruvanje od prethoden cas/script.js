70fd89bf59747e246c83ba52cd21b181644c7e2d
Peckoo <117806798+Peckoo@users.noreply.github.com>
2022-12-19 18:55:22 +0100

    Add files via upload

---
+++ Povtoruvanje od prethoden cas/script.js
@@ -0,0 +1,37 @@
+// let arr = [
+//     "Pece",
+//     "Good",
+//     "Working"
+// ]
+
+// function tellStory(array){
+// let result = `This is ${array[0]}. ${array[0]} is a nice person. 
+// Today they are ${array[1]}. 
+// They are ${array[2]} all day. The end.`
+
+// return result
+// }
+
+// let a = tellStory(arr);
+// console.log(a);
+
+
+//========================================================================
+
+// let names = ["Bob", "Jill"];
+// let surname = ["Bobski", "Jilski"];
+
+// let fullNames = [];
+// for(let i = 0; i < names.length; i++){
+//     let result = (i + 1) + ". " + names[i] + " " + surname[i];
+//     let result2 = `${ i + 1} ${names[i]} ${surname[i]}`
+//     // fullNames.push(result2);
+//     fullNames[i] = result
+//     console.log(result2)
+// }
+
+
+
+//=========================================================================
+
+
